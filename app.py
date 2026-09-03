5d4230fdff10f7b968cc45fa574eebd977096285
Guenter Klemp <g.klemp@gmx.de>
2026-09-03 11:48:41 +0200

    Add user and movie routes with templates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 from data_manager import DataManager
 from models import db, Movie
@@ -17,8 +17,28 @@ data_manager = DataManager()
 
 
 @app.route('/')
-def home():
-    return "Welcome to MoviWeb App!"
+def index():
+    users = data_manager.get_users()
+    return render_template('index.html', users=users)
+
+
+@app.route('/users')
+def list_users():
+    users = data_manager.get_users()
+    return str(users)
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    name = request.form.get('name')
+    data_manager.create_user(name)
+    return redirect('/')
+
+
+@app.route('/users/<int:user_id>/movies')
+def get_movies(user_id):
+    movies = data_manager.get_movies(user_id)
+    return render_template('movies.html', movies=movies)
 
 
 if __name__ == '__main__':
