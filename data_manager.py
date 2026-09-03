414787476cc19016a8d85fb1d4fe937b05f686ee
Guenter Klemp <g.klemp@gmx.de>
2026-09-03 09:51:29 +0200

    Add database models and data manager

---
+++ data_manager.py
@@ -0,0 +1,29 @@
+from models import db, User, Movie
+
+
+class DataManager:
+    """Handle database CRUD operations."""
+
+    def create_user(self, name):
+        """Create a new user."""
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+
+    def get_users(self):
+        """Return a list of all users."""
+        return User.query.all()
+
+    def get_movies(self, user_id):
+        """Return all movies for a specific user."""
+        return Movie.query.filter_by(user_id=user_id).all()
+
+    def add_movie(self, movie):
+        """Add a new movie to the database."""
+        db.session.add(movie)
+        db.session.commit()
+
+    def update_movie(self, movie_id, new_title):
+        """Update a movie by its id."""
+        Movie.query.filter_by(id=movie_id).update({"name": new_title})
+        db.session.commit()
